c0c87613720f065f39b51b939d41382f12acd404
dedani4 <dedani4@gmail.com>
2023-04-07 22:54:34 +0300

    add homework_3

---
+++ homework_3/1/1.js
@@ -0,0 +1,8 @@
+
+
+let gradus = +prompt("Input degree on the Celsius scale");
+alert(`${gradus} Celsius is ${transfer(gradus)} Fahrenheit`);
+
+function transfer(gradus) {
+    return Math.round(9 * gradus / 5 + 32);
+}
---
+++ homework_3/2/2.js
@@ -0,0 +1,8 @@
+
+
+const name1 = prompt("Input your name");
+greetings(name1);
+
+function greetings(name) {
+    console.log(`Hello, dear ${name}`);
+}

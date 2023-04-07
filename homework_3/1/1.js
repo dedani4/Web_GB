03cbc568fe292a4d751eb031c4a0dca786547e75
dedani4 <dedani4@gmail.com>
2023-04-07 23:16:04 +0300

    homework_3 task_1

--- homework_3/1/1.js
+++ homework_3/1/1.js
@@ -3,7 +3,6 @@
 let gradus = +prompt("Input degree on the Celsius scale");
 alert(`${gradus} Celsius is ${transfer(gradus)} Fahrenheit`);
 
-
 function transfer(gradus) {
     return Math.round(9 * gradus / 5 + 32);
 }
